1e069e5657052de1e718f4ab6446a37e4459d9e4
Machine100 <joesmileyyy@yahoo.com>
2020-06-24 14:49:36 -0400

    Stable working version
    Need to clean up code and add in a point to this project.

--- src/app/algorithms/bubblesort.service.ts
+++ src/app/algorithms/bubblesort.service.ts
@@ -11,11 +11,15 @@ export class BubblesortService {
   }
 
   gridOfColors: string[][]
-  // sortMa: number[]
+  row: number               // the row currently being sorted
+  bubbleSortComparisons     // count of comparisons done for bubble sort
+  mergeSortComparisons      // count of comparsions done for merge sort
 
   initBoard() {
     console.log('at initBoard')
-    // this.sortMa = []
+    this.row = 0             // only used for mergesort
+    this.bubbleSortComparisons = 0
+    this.mergeSortComparisons = 0
     this.gridOfColors = [ [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [],
                           [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [],
                           [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [],
@@ -66,6 +70,7 @@ export class BubblesortService {
   }
 
   async sortRowColors() {
+    this.bubbleSortComparisons = 0
     // console.log('at sortRowColors')
     for (let i = 0; i < 64; i++) {
       for (let j = 0; j < 63; j++) {
@@ -73,6 +78,7 @@ export class BubblesortService {
           const leftNumber: number = parseInt(this.gridOfColors[i][column], 16)
           const rightNumber: number = parseInt(this.gridOfColors[i][column + 1], 16)
           // console.log ('left:', leftNumber, 'right:', rightNumber)
+          this.bubbleSortComparisons++
           if (leftNumber > rightNumber) {
             const temp: string = this.gridOfColors[i][column]
             this.gridOfColors[i][column] = this.gridOfColors[i][column + 1]
@@ -89,78 +95,84 @@ export class BubblesortService {
 
           }
         }
-        await this._delayTimer()
+         // await this._delayTimer()
+      }
+      await this._delayTimer()
+    }
+    console.log('bubbleSortComparisons:', this.bubbleSortComparisons)
+  }
+
+  async doMergeSort() {
+    this.row = 0
+    this.mergeSortComparisons = 0
+    for (let i = 0; i < 64; i ++) {
+      this.mergeSort(this.gridOfColors[this.row])
+      // console.log(this.gridOfColors[this.row])
+      this.row++
+      // console.log('this.row', this.row)
+      await this._delayTimer()
+    }
+    console.log('mergeSortComparisons:', this.mergeSortComparisons)
+  }
+
+  async mergeSort(referenceTosortMA: string[], pointerStart?: number, pointerEnd?: number) {
+    // await this._delayTimer()
+    if (pointerStart === undefined) { pointerStart = 0}
+    if (pointerEnd === undefined) { pointerEnd = this.gridOfColors[this.row].length - 1 }
+    // console.log('pointerStart:', pointerStart, ' pointerEnd: ', pointerEnd)
+
+    if (pointerStart === pointerEnd) {             // check for base case of single digit
+        this._combine(referenceTosortMA, pointerStart, pointerEnd);
+        return
+    }
+    const pointerMiddle: number = Math.ceil( (pointerStart + pointerEnd) / 2 )
+    this.mergeSort(this.gridOfColors[this.row], pointerStart, pointerMiddle - 1)      // pass left side
+    this.mergeSort(this.gridOfColors[this.row], pointerMiddle, pointerEnd)
+    this._combine(this.gridOfColors[this.row], pointerStart, pointerEnd)
+    return
+  }
+
+  async _combine(referenceTosortMa: string[], leftStart: number, rightEnd: number) {
+    //await this._delayTimer()
+    if (leftStart === rightEnd) { return }
+
+    const rightStart: number = Math.ceil( (leftStart + rightEnd) / 2 )
+    const leftEnd: number = rightStart - 1
+    //console.log('combine:', 'LS:', leftStart, 'LE', leftEnd, 'RS', rightStart, 'RE', rightEnd)
+    let leftIndex: number = leftStart
+    let rightIndex: number = rightStart
+
+    const tempMa: string[] = []
+    while (leftIndex <= leftEnd && rightIndex <= rightEnd) {
+      const leftNumber: number = parseInt(this.gridOfColors[this.row][leftIndex], 16)
+      const rightNumber: number = parseInt(this.gridOfColors[this.row][rightIndex ], 16)
+      //console.log('leftIndex:', leftIndex, 'rightIndex', rightIndex)
+      //console.log('leftNumber: ',leftNumber, 'rightNumber: ', rightNumber)
+      if (leftNumber <= rightNumber) {
+        this.mergeSortComparisons++
+        tempMa.push(this.gridOfColors[this.row][leftIndex])
+        leftIndex++
+      } else {
+        tempMa.push(this.gridOfColors[this.row][rightIndex])
+        rightIndex++
       }
     }
-    console.log(this.gridOfColors)
-  }
-
-  doMergeSort() {
-    // this.sortMa = this.gridOfColors[1]
-    for (let row=0; row<10; row++) {
-    this.mergeSort(this.gridOfColors[row])
-    console.log(this.gridOfColors[row])
-  }
-}
-
-  mergeSort(referenceTosortMA: string[], pointerStart?: number, pointerEnd?: number) {
-      if (pointerStart === undefined) { pointerStart = 0}
-      if (pointerEnd === undefined) { pointerEnd = this.gridOfColors[1].length - 1 }
-      console.log('pointerStart:', pointerStart, ' pointerEnd: ', pointerEnd)
-
-      if (pointerStart === pointerEnd) {             // check for base case of single digit
-          this._combine(referenceTosortMA, pointerStart, pointerEnd);
-          return
-      }
-      const pointerMiddle: number = Math.ceil( (pointerStart + pointerEnd) / 2 )
-      this.mergeSort(this.gridOfColors[1], pointerStart, pointerMiddle - 1)      // pass left side
-      this.mergeSort(this.gridOfColors[1], pointerMiddle, pointerEnd)
-      this._combine(this.gridOfColors[1], pointerStart, pointerEnd)
-      return
-  }
-
-  _combine(referenceTosortMa: string[], leftStart: number, rightEnd: number) {
-      if (leftStart === rightEnd) { return }
-
-      const rightStart: number = Math.ceil( (leftStart + rightEnd) / 2 )
-      const leftEnd: number = rightStart - 1
-      //console.log('combine:', 'LS:', leftStart, 'LE', leftEnd, 'RS', rightStart, 'RE', rightEnd)
-      let leftIndex: number = leftStart
-      let rightIndex: number = rightStart
-
-      const tempMa: string[] = []
-      while (leftIndex <= leftEnd && rightIndex <= rightEnd) {
-        const leftNumber: number = parseInt(this.gridOfColors[1][leftIndex], 16)
-        const rightNumber: number = parseInt(this.gridOfColors[1][rightIndex ], 16)
-        //console.log('leftIndex:', leftIndex, 'rightIndex', rightIndex)
-        //console.log('leftNumber: ',leftNumber, 'rightNumber: ', rightNumber)
-        if (leftNumber <= rightNumber) {
-            //console.log('left is lower')
-            tempMa.push(this.gridOfColors[1][leftIndex])
-            leftIndex++
-        } else {
-          //console.log('left not lower')
-            tempMa.push(this.gridOfColors[1][rightIndex])
-            rightIndex++
-        }
-      }
-      while (leftIndex <= leftEnd) {    // if anything left on left side push them all onto end of stack
-          tempMa.push(this.gridOfColors[1][leftIndex])
-          leftIndex++
-      }
-      while (rightIndex <= rightEnd) {
-          tempMa.push(this.gridOfColors[1][rightIndex])
-          rightIndex++
-      }
-      //console.log('tempMa:', tempMa)
-      for (let i = leftStart; i <= rightEnd; i++) {
-        this.gridOfColors[1][i] = tempMa.shift()
-
-        const id: string = '1' + '_' + i
-        const toColor: string = '#' + this.gridOfColors[1][i]
-        document.getElementById(id).style.backgroundColor = toColor
-
-      }
+    while (leftIndex <= leftEnd) {    // if anything left on left side push them all onto end of stack
+      tempMa.push(this.gridOfColors[this.row][leftIndex])
+      leftIndex++
+    }
+    while (rightIndex <= rightEnd) {
+      tempMa.push(this.gridOfColors[this.row][rightIndex])
+      rightIndex++
+    }
+    //console.log('tempMa:', tempMa)
+    for (let i = leftStart; i <= rightEnd; i++) {                  // Merge temporary array into into main array
+      this.gridOfColors[this.row][i] = tempMa.shift()
+      const id: string = this.row.toString() + '_' + i
+      const toColor: string = '#' + this.gridOfColors[this.row][i]
+      document.getElementById(id).style.backgroundColor = toColor
+      //await this._delayTimer()
+    }
 
       //console.log('sortMa:', this.gridOfColors[1])
       return
